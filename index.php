7024d37c7d9d67bf7ef285d6627b5b4231ebee42
borquez <diegoborquez1@hotmail.com>
2015-07-08 17:14:10 -0400

    cambie un poco el formato de la pag

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 
 <html>
 	<head>
-		<?php
-		session_start();
-		?>
+		
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title>Diego's web</title>
 	</head>
@@ -13,40 +11,101 @@
 		<u><strong><h1>"lA PAGINA DE DIEGO BORQUEZ "</h1></strong></u>
 		
 		<hr>
-		<form method="POST" action="lista.php" >
+		<form method="POST" action="index.php" >
 		<table>
 		<tr>
-		
+		<td><h4>Ramo</h4></td>
+		<td><h4>Profesor</h4></td>
+		<td><h4>Prueba</h4></td>
+		<td><h4>Universidad</h4></td>
+		<td><h4>Facultad</h4></td>
 		</tr>
   		<tr>
-    <td><input type="text" name="Ramo" value="Ramo" /> <br/>	</td>
-    <td><input type="text" name="Profesor" value=" Nombre de profesor" /> <br/></td>
-    <td><input type="text" name="Prueba" value=" Prueba" /> <br/></td>
-    <td><input type="text" name="Universidad" value=" Universidad" /> <br/></td>
-    <td><input type="text" name="Facultad" value=" Facultad" /> <br/></td>
-    <td><input type="submit" value="Ingresar" /></td>
+ 
+
+<?php 
+  /*Conexion a la bd*/
+$link = mysql_connect('localhost', 'root', '');
+$db_selected = mysql_select_db('apoyouniversitario',$link);
+$dat="SELECT DISTINCT Ramo FROM Apoyo";
+$sql=mysql_query($dat);
+ ?>
+
+<td><select name="Ramo">
+<option selected="selected" value="vacio"></option>
+ <?php
+ while($lista=mysql_fetch_array($sql))
+   echo "<option  value='".$lista["Ramo"]."'>".$lista["Ramo"]."</option>"; 
+?>
+</select></td>
+<?php 
+  /*Conexion a la bd*/
+$link = mysql_connect('localhost', 'root', '');
+$db_selected = mysql_select_db('apoyouniversitario',$link);
+$dat="SELECT DISTINCT Profesor FROM Apoyo";
+$sql=mysql_query($dat);
+ ?>
+<td><select name="Profesor">
+<option selected="selected" value="vacio"></option>
+ <?php
+ while($lista1=mysql_fetch_array($sql))
+   echo "<option  value='".$lista1["Profesor"]."'>".$lista1["Profesor"]."</option>"; 
+?>
+</select></td>
+<?php 
+  /*Conexion a la bd*/
+$link = mysql_connect('localhost', 'root', '');
+$db_selected = mysql_select_db('apoyouniversitario',$link);
+$dat="SELECT DISTINCT Prueba FROM Apoyo";
+$sql=mysql_query($dat);
+ ?>
+<td><select name="Prueba">
+<option selected="selected" value="vacio"></option>
+ <?php
+ while($lista=mysql_fetch_array($sql))
+   echo "<option  value='".$lista["Prueba"]."'>".$lista["Prueba"]."</option>"; 
+?>
+</select></td>
+<?php 
+  /*Conexion a la bd*/
+$link = mysql_connect('localhost', 'root', '');
+$db_selected = mysql_select_db('apoyouniversitario',$link);
+$dat="SELECT DISTINCT Universidad FROM Apoyo";
+$sql=mysql_query($dat);
+ ?>
+<td><select name="Universidad">
+<option selected="selected" value="vacio"></option>
+ <?php
+ while($lista=mysql_fetch_array($sql))
+   echo "<option  value='".$lista["Universidad"]."'>".$lista["Universidad"]."</option>"; 
+?>
+</select></td>
+<?php 
+  /*Conexion a la bd*/
+$link = mysql_connect('localhost', 'root', '');
+$db_selected = mysql_select_db('apoyouniversitario',$link);
+$dat="SELECT DISTINCT Facultad FROM Apoyo";
+$sql=mysql_query($dat);
+ ?>
+<td><select name="Facultad">
+<option selected="selected" value="vacio"></option>
+<option selected="selected" value="vacio"></option>
+ <?php
+ while($lista=mysql_fetch_array($sql))
+   echo "<option  value='".$lista["Facultad"]."'>".$lista["Facultad"]."</option>"; 
+?>
+</select></td>
+    <!---<td><input type="text" name="Profesor" placeholder=" Nombre de profesor" /> <br/></td>
+    <td><input type="text" name="Prueba" placeholder=" Prueba" /> <br/></td>
+    <td><input type="text" name="Universidad" placeholder=" Universidad" /> <br/></td>
+    <td><input type="text" name="Facultad" placeholder=" Facultad" /> <br/></td>-->
+    <td><input type="submit" value="Ingresar" name='Ingresar' /></td>
   		</tr>
-  		<tr>
-  		
-		</table>
-		 
-		
+  				
+		</table>		 		
 		</form>
 		<form method="POST" action="subir.php">
 		<input type="submit" value="subir">
-		</form>
-<?php
-include_once 'config.inc.php';
-$db= new Connect_Mysql()
-$ramo=$_POST['Ramo']
-$profesor=$_POST['Profesor']
-$prueba=$_POST['Prueba']
-$universidad=$_POST['Universidad']
-$facultad=$_POST['Facultad']
-
-?>
-
-			
-	
+		</form>			
 	</body>
 </html>
